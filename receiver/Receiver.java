c4556d0053c8170a09be5454bf9a5f8a29c9e7a9
mathieu perron <math.per@hotmail.com>
2019-11-17 21:02:49 -0500

    starting stuff

---
+++ receiver/Receiver.java
@@ -0,0 +1,10 @@
+/* the receiver has to be able to:
+   1 - receive trams
+   2 - verify presence of errors
+   3 - produce and send receipts
+   4 - send REJ in case of errors
+ */
+
+public class Receiver {
+
+}
---
+++ transmitter/Transmitter.java
@@ -0,0 +1,10 @@
+/* the transmitter has to :
+   1 - read file's data
+   2 - product and send trams
+   3 - handle receipts
+   4 - re-send the data in case of error
+ */
+
+public class Transmitter {
+
+}
