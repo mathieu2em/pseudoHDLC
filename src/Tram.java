5db7061cf5677997f9e33b2f58a2551d24bf40e4
mathieu perron <math.per@hotmail.com>
2019-11-17 21:13:13 -0500

    add tram class and some infos

---
+++ src/Tram.java
@@ -0,0 +1,10 @@
+/* structure is : | Flag | Type | Num | Data | CRC | Flag |
+   flag : 1 octet (01111110)
+   Type : can be I , C , A, R, F, P
+   Num  : 1 octet specify the number of the tram sent (I) or the receipt nbr (RR,REJ)
+   Data : variable size , used to carry data, if receipt tram : size = null , size calculated by detecting flags
+   CRC  : contains the checksum computed using CRC , 2 octets, checksum is calculated on Type, Num and Data
+ */
+public class Tram {
+
+}
