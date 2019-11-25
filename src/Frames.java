336f7b9cd58ea0c3607c80300036e3cdf261fc70
song-y <song-yumi@hotmail.com>
2019-11-25 14:21:54 -0500

    add 2 methods : toByteArray, DivideByCRC

--- src/Frames.java
+++ src/Frames.java
@@ -1,5 +1,4 @@
 import jdk.nashorn.internal.runtime.BitVector;
-
 import java.util.ArrayList;
 
 /* structure is : | Flag | Type | Num | Data | CRC | Flag |
@@ -12,8 +11,7 @@ import java.util.ArrayList;
 public class Frames {
 
     private byte flag = 0b01111110;
-    private BitVector RCR = new BitVector();
-    // private byte[] generator = [0b10001000000100001];
+    private int[] CRC = {1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,1};
     /*
     private byte I;
     private byte C;
@@ -46,9 +44,9 @@ public class Frames {
     }
 
     // constructeur pour les frames qui n'ont pas besoin de data
-    public Frames(Character type){
+    public Frames(Character type) {
         // if connection demand , num=0 to ask for Go-Back-N
-        if (type=='C'){
+        if (type == 'C') {
             this.Num = 0b00000000; // Num= 0 means that we ask for Go-Back-N
         }
     }
@@ -57,22 +55,18 @@ public class Frames {
     //private <type> CRC;
 
 
-
-
     // TODO trouver une facon d'assembler correctement la tram pour l'envoi selon la structure plus haut
-    public ArrayList<Byte> toByteArray(String lineOfFrame)
-    {
+    public ArrayList<Byte> toByteArray(String lineOfFrame) {
         byte[] arrayOfByte = lineOfFrame.getBytes();
 
         ArrayList<Byte> byteArrayList = new ArrayList<>();
 
         //here we add the flag, type and num for each frame
         byteArrayList.add(this.flag);
-        byteArrayList.add((byte)this.type);
+        byteArrayList.add((byte) this.type);
         byteArrayList.add(this.Num);
 
-        for (int i = 0; i < arrayOfByte.length; i++ )
-        {
+        for (int i = 0; i < arrayOfByte.length; i++) {
             byteArrayList.add(arrayOfByte[i]);
         }
 
@@ -82,32 +76,57 @@ public class Frames {
         return arrayOfByte;
     }
 
-    public int[] divideByCRC(int messageToEncode[], int CRC[])
+    public int[] getEncodedMessage(int messageToEncode[])
     {
+        int remainder[] = divideByCRC(messageToEncode);
+        for(int i=0 ; i < messageToEncode.length ; i++) {
+            System.out.print(messageToEncode[i]);
+        }
+        for(int i=0 ; i < remainder.length-1 ; i++) {
+            System.out.print(remainder[i]);
+        }
+        //return
+    }
+
+    public int[] divideByCRC(int messageToEncode[]) {
         int remainder[];
+
         int data[] = new int[messageToEncode.length + CRC.length];
         System.arraycopy(messageToEncode, 0, data, 0, messageToEncode.length);
 
+        //array that stores the remainder. remainder's bits initially set to the data bits
         remainder = new int[CRC.length];
         System.arraycopy(data, 0, remainder, 0, CRC.length);
 
-        for (int i = 0; i < messageToEncode.length; )
-
-    }
-    /*public byte[] computeCRC(ArrayList<Byte> arrayListOfByte)
-    {
-        byte[] byteArray = new byte[arrayListOfByte.size()];
-        ArrayList<Boolean> encodedMessage = new ArrayList<>();
-        for (int i = 0; i < byteArray.length; i++)
-        {
-            //byteArray[i] = (byte) arrayListOfByte.get(i);
-            if (byteArray[i] != generator[i]) //XOR result is 1, add 1
-                encodedMessage.add(true);
-            else                        //XOR result is 0, add 0
-                encodedMessage.add(false);
+        //loop continuously EXOR the bits of the remainder (data) and CRC
+        for (int i = 0; i < messageToEncode.length; i++) {
+            if (remainder[0] == 1) {
+                for (int j = 1; j < CRC.length; j++) {
+                    remainder[j - 1] = exor(remainder[j], CRC[j]);
+                }
+            } else {
+                for (int j = 1; j < CRC.length; j++) {
+                    remainder[j - 1] = exor(remainder[j], 0);
+                }
+            }
+            remainder[CRC.length - 1] = data[i + CRC.length];
         }
+        return remainder;
+    }
 
+    //XOR operation with 2 bits given in entry
+    public int exor(int a, int b) {
+      /*  if (a == b) {
+            return 0;
+        }*/
+        return a^b;
+    }
 
-    }*/
+    private int[] appendRemainderToData(int[] remainder, int[] data) {
+        int[] encodedMessage = new int[data.length + remainder.length];
+        System.arraycopy(data, 0, encodedMessage, 0, data.length);
+        System.arraycopy(remainder, 0, encodedMessage, data.length, remainder.length);
 
+        return encodedMessage;
+    }
 }
